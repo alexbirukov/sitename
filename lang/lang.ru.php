ffc45fbd3b98aa185b2e476a9aacdbc1bfb42eb8
alexbirukov <lord_maloy@mail.ru>
2012-12-10 23:41:25 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,24 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-10-18'
+	);
+
+	/**
+	 * Site Name
+	 */
+	$dictionary = array(
+
+		'Site Name' => 
+		'Имя сайта',
+
+		'Website Name' => 
+		'Укажите название сайта',
+
+	);
